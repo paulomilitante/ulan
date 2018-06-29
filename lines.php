1ea5bebee99ce8aef8b09817768097163f92304a
Paulo Militante <p.militante@creativehope.jp>
2018-06-29 14:53:12 +0800

    added witty lines

--- lines.php
+++ lines.php
@@ -1,9 +1,19 @@
 <?php 
 
 $hindi = ["Pero magdala ka pa rin ng payong kung ayaw mong mangitim.",
-		"Pero as usual, siguradong traffic pa rin."];
+		"Pero as usual, siguradong traffic pa rin.",
+		"Kung may kotse ka, pwede ka magpa-carwash ngayon.",
+		"Pero kung makulimlim, magdala ka na rin ng payong para sigurado.",
+		"Pwedeng pumorma, hindi mababasa ang KOTD mo.",
+		"Pero pwede ka pa rin magdala ng payong. Meron tayong free will, gamitin natin ito.",
+		"Di mo na kailangan magdala ng payong. Ingat palagi!"];
 
 $oo = ["Magdala ka ng payong o kapote kung ayaw mong mabasa.",
-	"Stay dry! Magastos magkasakit. Bawal magkasakit."];
+	"Stay dry! Magastos magkasakit. Bawal magkasakit.",
+	"Umaaraw, umuulan. Ang buhay ay sadyang ganyan.",
+	"Lagi na lang umuulan, parang walang katapusan.",
+	"Pagmasdan ang ulan, unti-unting pumapatak sa mga halama't mga bulaklak.",
+	"Pumapatak na naman ang ulan sa bubong ng bahay.",
+	"Wag mo muna ipa-carwash yang kotse mo. Baka masayang ang effort."];
 
 ?>
